9b5056ed2064e1282dca9390ba58088c11778f7e
qilixuening <qilixuening@hotmail.com>
2021-10-01 13:17:57 +0800

    登录模块验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,29 +1,34 @@
 <template>
   <div class="login-container">
-    <el-form class="login-form" ref="form" :model="user">
-      <el-form-item>
+    <el-form
+      class="login-form"
+      ref="login-form"
+      :model="user"
+      :rules="loginFormRules"
+    >
+      <el-form-item prop="mobile">
         <el-input
           v-model="user.mobile"
           placeholder="请输入手机号"
         ></el-input>
       </el-form-item>
-      <el-form-item>
+      <el-form-item prop="code">
         <el-input
           v-model="user.code"
           placeholder="请输入验证码"
         ></el-input>
       </el-form-item>
-      <el-form-item>
+      <el-form-item prop="checked">
         <el-checkbox
-          v-model="checked"
-        >我已阅读并同意<a href="#">用户许可及隐私条款</a>。</el-checkbox>
+          v-model="user.checked"
+        >我已阅读并同意用户许可及隐私条款。</el-checkbox>
       </el-form-item>
       <el-form-item>
         <el-button
           class="login-btn"
           type="primary"
           :loading="loginLoading"
-          @click="onLogin"
+          @click="onLogin('login-form')"
         >立即创建</el-button>
       </el-form-item>
     </el-form>
@@ -37,36 +42,64 @@ export default {
   name: 'LoginIndex',
   components: {},
   props: {},
-  data () {
-    return {
-      user: {
-        mobile: '',
-        code: '',
-        loginLoading: false
-      },
+  data: () => ({
+    user: {
+      mobile: '',
+      code: '',
       checked: false
-    }
-  },
+    },
+
+    loginFormRules: {
+      mobile: [
+        { required: true, message: '手机号不能为空', trigger: 'blur' },
+        { pattern: /^1[3-9]\d{9}$/, message: '手机号无效', trigger: 'blur' },
+        { pattern: /^\d+$/, message: '手机号只包含数字', trigger: 'change' }
+      ],
+      code: [
+        { required: true, message: '验证码不能为空', trigger: 'blur' },
+        { pattern: /^\d{6}$/, message: '验证码无效', trigger: 'blur' }
+      ],
+      checked: [
+        {
+          validator: (_, value, callback) => {
+            if (!value) {
+              callback(new Error('请阅读并同意用户协议'))
+            } else {
+              callback()
+            }
+          },
+          trigger: 'change'
+        }
+      ]
+    },
+
+    loginLoading: false
+  }),
   computed: {},
   watch: {},
   created () {},
   mounted () {},
+
   methods: {
-    onLogin () {
-      const user = this.user
+    onLogin (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.login()
+        }
+      })
+    },
+
+    login () {
       this.loginLoading = true
       request({
         method: 'POST',
         url: 'mp/v1_0/authorizations',
-        data: user
+        data: this.user
       }).then(res => {
-        console.log(res)
-
         this.$message({
           message: '登录成功！',
           type: 'success'
         })
-
         this.loginLoading = false
       }).catch(err => {
         console.log('登录失败\n', err)
