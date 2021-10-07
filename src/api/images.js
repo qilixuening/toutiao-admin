06864228428fcc1aa1558f29ffd4cf69f2771ee6
qilixuening <qilixuening@hotmail.com>
2021-10-07 22:09:06 +0800

    完成素材管理全部功能

--- src/api/images.js
+++ src/api/images.js
@@ -12,12 +12,26 @@ export const uploadImage = data => {
 }
 
 export const getUserImages = params => {
-  // 文件上传一般需要设置Content-Type为multipart/form-data
-  // 但axios上传文件无需手动设置，只需要使data作为一个FormData即可
-  // new FormData()
   return request({
     method: 'GET',
     url: 'mp/v1_0/user/images',
     params
+  })
+}
+
+export const alterImageInfo = (imageId, collect) => {
+  return request({
+    method: 'PUT',
+    url: `mp/v1_0/user/images/${imageId}`,
+    data: {
+      collect
+    }
+  })
+}
+
+export const deleteImage = imageId => {
+  return request({
+    method: 'DELETE',
+    url: `mp/v1_0/user/images/${imageId}`
   })
 }
--- src/views/images/index.vue
+++ src/views/images/index.vue
@@ -12,7 +12,7 @@
         style="padding-bottom: 20px"
         v-model="isFavorite"
         size="mini"
-        @change="loadUserImages">
+        @change="loadUserImages(1)">
         <el-radio-button :label="false">全部</el-radio-button>
         <el-radio-button :label="true">收藏</el-radio-button>
       </el-radio-group>
@@ -26,6 +26,7 @@
 
       <el-row :gutter="10">
         <el-col
+          class="img-box"
           :xs="12"
           :sm="8"
           :md="6"
@@ -38,10 +39,43 @@
             style="height: 200px"
             :src="img.url"
             fit="cover"
+            lazy
           >
           </el-image>
+          <div class="img-bar">
+            <el-button
+              class="op-button"
+              type="plain"
+              circle
+              :icon="img.is_collected ? 'el-icon-star-on' : 'el-icon-star-off'"
+              :loading="img.loading"
+              @click="onAlterImageInfo(img)"
+            >
+            </el-button>
+            <el-button
+              class="op-button"
+              type="plain"
+              icon="el-icon-delete"
+              circle
+              :loading="img.loading"
+              @click="onDeleteImage(img)"
+            >
+            </el-button>
+          </div>
         </el-col>
       </el-row>
+
+      <el-pagination
+        class="pagination"
+        layout="prev, pager, next"
+        background
+        :total="totalCount"
+        :page-size="pageSize"
+        :disabled="loading"
+        :current-page.sync="page"
+        @current-change="onPageChange"
+      >
+      </el-pagination>
     </el-card>
 
     <el-dialog
@@ -69,17 +103,23 @@
 </template>
 
 <script>
-import { getUserImages } from '@/api/images'
+import { getUserImages, alterImageInfo, deleteImage } from '@/api/images'
 export default {
   name: 'ImagesIndex',
   components: {},
   props: {},
   data: () => ({
+    loading: false,
+
     isFavorite: false,
     isShowUploadDialog: false,
     uploadHeaders: {
       Authorization: `Bearer ${JSON.parse(window.localStorage.getItem('user')).token}`
     },
+
+    page: 1,
+    pageSize: 24,
+    totalCount: 0,
 
     images: []
   }),
@@ -90,12 +130,24 @@ export default {
   },
   mounted () {},
   methods: {
-    loadUserImages () {
+    loadUserImages (page = 1) {
+      this.loading = true
+
+      this.page = page
       getUserImages({
-        collect: this.isFavorite
+        collect: this.isFavorite,
+        page,
+        per_page: this.pageSize
       }).then(res => {
-        this.images = res.data.data.results
+        const ret = res.data.data.results
+        ret.forEach(img => {
+          img.loading = false
+        })
+        this.images = ret
+        this.totalCount = res.data.data.total_count
       })
+
+      this.loading = false
     },
 
     ShowUploadDialog () {
@@ -104,19 +156,70 @@ export default {
 
     onUploadSuccess () {
       this.loadUserImages()
+
+      this.$message({
+        type: 'success',
+        message: '上传成功！'
+      })
       this.isShowUploadDialog = false
     },
 
     onClosingDialog (done) {
-      this.$confirm('确认关闭？')
+      this.$confirm('是否暂停上传？')
         .then(_ => {
           done()
         })
         .catch(_ => {})
+    },
+
+    onPageChange (page) {
+      this.loadUserImages(page)
+    },
+
+    onAlterImageInfo (img) {
+      img.loading = true
+      alterImageInfo(img.id, !img.is_collected).then(res => {
+        img.loading = false
+        img.is_collected = !img.is_collected
+      })
+    },
+
+    onDeleteImage (img) {
+      img.loading = true
+      deleteImage(img.id).then(res => {
+        this.loadUserImages(this.page)
+      })
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
+  .img-box {
+    position: relative;
+
+    .img-bar {
+      height: 24px;
+      background-color: rgba(0, 0, 0, 0.3);
+      position: absolute;
+      bottom: 4px;
+      left: 5px;
+      right: 5px;
+      display: flex;
+      justify-content: space-evenly;
+      align-items: center;
+
+      .op-button {
+        padding: 0;
+        background: none;
+        border: none;
+        font-size: 18px;
+        color: rgb(235, 233, 233);
+      }
+    }
+  }
+
+  .pagination {
+    margin-top: 20px;
+  }
 </style>
